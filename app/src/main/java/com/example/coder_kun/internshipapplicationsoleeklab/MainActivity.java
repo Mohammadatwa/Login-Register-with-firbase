ffb344a5b2c74e702043dbb258b16a2d4b334211
Mohammadatwa <mohammad.a.atwa@gmail.com>
2019-01-23 19:41:21 +0200

    commiting task

---
+++ app/src/main/java/com/example/coder_kun/internshipapplicationsoleeklab/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.coder_kun.internshipapplicationsoleeklab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
